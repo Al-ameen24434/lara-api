4d094180146399fbdcdd0da7ded5fa5f4b1673a5
Al-ameen24434 <alameenabdulrahmon@gmail.com>
2026-04-27 23:29:53 +0100

    update task module

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,7 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Task extends Model
 {
-    //
+    
+    use HasFactory;
+
+    /**
+          *
+     * @var array<string>
+     * The <string> in the comment means this is an array of strings
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'status',
+        'due_date',
+    ];
+
+    /**
+     
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'due_date' => 'datetime',
+    ];
+
+   
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+      
+    }
 }
